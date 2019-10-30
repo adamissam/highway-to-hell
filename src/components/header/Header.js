10084d4f01958a60e2e82eda6e5673f085ce3316
marie <marigory0@gmail.com>
2019-10-30 21:57:13 +0100

    dernieres modif header

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className ="titlebar">
+        <h1 className ="title">  HighWay To Hell </h1>
+        
+        <p className="introTitle"> The best selection of Creepy, bloody, scary movies for your Halloween night</p>
+        
+             <ul>
+                <li><a href="#Movies" class="button-hover"> Movies</a></li>
+                <li><a href="#MonsterGame" class="button-hover"> MonsterGame</a></li>
+            </ul>
+        
+        </div>
+    );
+}
+
+export default Header;
