618f6575cfd0b6519dde3303b40db903180c1870
kinou-wild <kevin.thomann13@gmail.com>
2019-10-31 06:38:08 +0100

    ébauche composant monsterfight

--- src/components/pageMonster/HomeMonster.jsx
+++ src/components/pageMonster/HomeMonster.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Footer from '../footer/Footer'
 import MonsterList from './MonsterList'
 import './HomeMonster.css'
+import MonsterFight from './MonsterFight'
 
-class HomeFilm extends React.Component {
+class HomeMonster extends React.Component {
     render(){
         return(
             <div className="homeMonsterDiv">
                 {/*<Header />*/}
                 <MonsterList />
+                <MonsterFight />
                 <Footer />
             </div>
         )
     }
 }
 
-export default HomeFilm
+export default HomeMonster
