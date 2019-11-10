86997d040f2732bcc793ea12f625dcdf8ed8599f
Adam Issam <issamadam15@gmail.com>
2019-11-10 11:01:42 +0100

    modification carousel

--- src/components/pageFilm/Carousel.js
+++ src/components/pageFilm/Carousel.js
@@ -1,142 +1,130 @@
-import React from "react";
-import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage,
-MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from "mdbreact";
-import './Carousel.css';
+import React, { useState,useEffect } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Button, Modal, ModalHeader, ModalBody, ModalFooter
+} from 'reactstrap';
 import  Axios from "axios";
+import './Carousel.css'
+import Trailer from './arrayTrailer'
+import ReactPlayer from 'react-player'
 
-class MultiCarouselPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            data: []
+
+const MultiCarouselPage = (props) => {
+          const {
+            buttonLabel,
+            className
+          } = props;
+
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+  const [dataApi, setDataApi] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [titre, setTitre] = useState('');
+  const [linki,setLinki] = useState('')
+  
+      
+  useEffect(() => {
+    
+  const getApi = async () =>{
+       const result =  await Axios("https://hackathon-wild-hackoween.herokuapp.com/movies")
+        setDataApi(result.data.movies)
         }
-    }
-    componentDidMount(){
-        Axios.get("https://hackathon-wild-hackoween.herokuapp.com/movies")
-        .then(Response => {
-            this.setState({data: Response.data})
-        })
-        .catch(err => console.log(err))
-    }
+       getApi()
+      }, [])
+  
 
-    render() {
-        return (
-            <MDBContainer className="cardImg">
-              <MDBCarousel activeItem={1} length={3} slide={true} showControls={true} showIndicators={true} multiItem>
-                <p className="scroller">Last movies</p>
-                <MDBCarouselInner>
-                  <MDBRow>
-                    <MDBCarouselItem itemId="1">
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/1910070355402418.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Hereditary</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007044131396219.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Blood_Island</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007040506771841.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Paranormal_Activity_2</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="2">
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007035726264614.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Us</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007041123380836.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Saw</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/19100704112336450.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Saw_2</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="3">
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007035914844273.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Scream_3</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007035954399300.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Scream_4</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                      <MDBCol md="4">
-                        <MDBCard className="mb-2">
-                          <MDBCardImage className="img-fluid" src="https://nsa40.casimages.com/img/2019/10/07/191007040324664897.jpg" />
-                          <MDBCardBody>
-                            <MDBCardTitle className="title">Insidious</MDBCardTitle>
-                            <MDBCardText>
-                            </MDBCardText>
-                            <MDBBtn color="light">More</MDBBtn>
-                          </MDBCardBody>
-                        </MDBCard>
-                      </MDBCol>
-                    </MDBCarouselItem>
-                  </MDBRow>
-                </MDBCarouselInner>
-              </MDBCarousel>
-            </MDBContainer>
-          );
-    }
-  }
+      const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === dataApi.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+      }
+
+      const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? dataApi.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+      }
+
+      const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+      }
+
+      const toggle = () => {
+        setModal(!modal);
+        
+       };
+      const saveTitre = (item) =>{
+        setTitre(item.title)
+        toggle()
+      }
+      const fetchLink = () =>{
+        const link = Trailer.find(e => e.title.toLowerCase() === titre.replace(/_/g,' ').toLowerCase())
+        return link && link.url
+      }
+
+  const slides = dataApi.splice(0,5).map((item) => {
+    return (
+      <CarouselItem
+        className="custom-tag"
+        tag="div"
+        key={item.id}
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+      >
+        <img src={item.posterUrl} alt={item.title} className='image' onClick={() =>saveTitre(item)}  />
+        
+        <CarouselCaption className="text-primary" captionText={item.year} captionHeader={item.title}  />
+        
+      </CarouselItem>
+    );
+  });
+console.log(titre)
+  return (
+    <div className='principalDiv'>
+      <style>
+        {
+          `.custom-tag {
+              max-width: 100%;
+              height: 700px;
+              background: white;
+            }`
+            
+        }
+      </style>
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+      >
+        <CarouselIndicators items={dataApi} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+
+      <Modal isOpen={modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
+        toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>{titre}</ModalHeader>
+        <ModalBody>
+        <ReactPlayer url={fetchLink()}playing />
+      
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+      
+
+    </div>
+  );
+}
+
 
 export default MultiCarouselPage;
---
+++ src/components/pageFilm/arrayTrailer.jsx
@@ -0,0 +1,333 @@
+import React, { Component } from 'react';
+const Trailer=[
+    {
+        title:'Hereditary',
+        url:'https://www.youtube.com/watch?v=V6wWKNij_1M'
+    },
+    {
+        title:'Us',
+        url:'https://www.youtube.com/watch?v=1qEQmpprzSM'
+    },
+    {
+        title:'Scream 2',
+        url:'https://www.youtube.com/watch?v=C-j2TLBmTBY&t=55s'
+    },
+    {
+        title:'Scream 3',
+        url:'https://www.youtube.com/watch?v=au0NU6YbN7Q'
+    },
+    {
+        title:'Scream 4',
+        url:'https://www.youtube.com/watch?v=JKRtyVLWV-E&t=89s'
+    },
+    {
+        title:'Paranormal Activity',
+        url:'https://www.youtube.com/watch?v=F_UxLEqd074'
+    },
+    {
+        title:'Final Destination',
+        url:'https://www.youtube.com/watch?v=lP4Psj7d1ZI'
+    },
+    {
+        title:'Final Destination 2',
+        url:'https://www.youtube.com/watch?v=_S9V0uFGKSs'
+    },
+    {
+        title:'Final Destination 3',
+        url:'https://www.youtube.com/watch?v=ectjqGg92M8'
+    },
+    {
+        title:'Final Destination 4',
+        url:'https://www.youtube.com/watch?v=bkmaPmf8Od0'
+    },
+    {
+        title:'Final Destination 5',
+        url:'https://www.youtube.com/watch?v=24abND3yDq0'
+    },
+    {
+        title:'Insidious',
+        url:'https://www.youtube.com/watch?v=zuZnRUcoWos'
+    },
+    {
+        title:'Esther',
+        url:'https://www.youtube.com/watch?v=r1nRTtmZnJY'
+    },
+    {
+        title:'Paranormal Activity 2',
+        url:'https://www.youtube.com/watch?v=07XbSk7Rjt4'
+    },
+    {
+        title:'REC',
+        url:'https://www.youtube.com/watch?v=YGJ_jPKOj1c'
+    },
+    {
+        title:'REC 2',
+        url:'https://www.youtube.com/watch?v=GGKNxtZLXIE'
+    },
+    {
+        title:'saw',
+        url:'https://www.youtube.com/watch?v=S-1QgOMQ-ls&t=52s'
+    },
+    {
+        title:'Saw 2',
+        url:'https://www.youtube.com/watch?v=Lq2AzZB6DUE'
+    },
+    {
+        title:'Saw 3',
+        url:'https://www.youtube.com/watch?v=eZwnZqKfvzw'
+    },
+    {
+        title:'Saw 4',
+        url:'https://www.youtube.com/watch?v=PYQ75b89PPw'
+    },
+    {
+        title:'Saw 5',
+        url:'https://www.youtube.com/watch?v=EnBCBttyRX8'
+    },
+    {
+        title:'Saw 6',
+        url:'https://www.youtube.com/watch?v=XKxcf50t1xE'
+    },
+    {
+        title:'Saw 3D',
+        url:'https://www.youtube.com/watch?v=IG9ZKtsz8Ww'
+    },
+    {
+        title:'Akmareul boatda',
+        url:'https://www.youtube.com/watch?v=8DYPUFEfe28'
+    },
+    {
+        title:'The Hills Have Eyes',
+        url:'https://www.youtube.com/watch?v=CUQd9OB75dw'
+    },
+    {
+        title:'A Nightmare on Elm Street',
+        url:'https://www.youtube.com/watch?v=dCVh4lBfW-c'
+    },
+    {
+        title:'Let Me In',
+        url:'https://www.youtube.com/watch?v=x3guLR8aBpU'
+    },
+    {
+        title:'Devil',
+        url:'https://www.youtube.com/watch?v=C7l3nzjHYTg'
+    },
+    {
+        title:'The Thing',
+        url:'https://www.youtube.com/watch?v=Txjm94GnrPA'
+    },
+    {
+        title:'The Last Exorcism',
+        url:'https://www.youtube.com/watch?v=F3cI0pArMA0'
+    },
+    {
+        title:'The Ward',
+        url:'https://www.youtube.com/watch?v=buF-keImNYk'
+    },
+    {
+        title:'The Crazies',
+        url:'https://www.youtube.com/watch?v=J7w9uWFIMBs'
+    },
+    {
+        title:'Scream',
+        url:'https://www.youtube.com/watch?v=AWm_mkbdpCA'
+    },
+    {
+        title:'I Spit on Your Grave',
+        url:'https://www.youtube.com/watch?v=HC9p7SkJPwE'
+    },
+    {
+        title:"Julia's Eyes",
+        url:'https://www.youtube.com/watch?v=nbLk_gI5Vdw&t=26s'
+    },
+    {
+        title:'Shelter',
+        url:'https://www.youtube.com/watch?v=bKsCvcvoZcE'
+    },
+    {
+        title:'Apollo 18',
+        url:'https://www.youtube.com/watch?v=sAWdCztPP3s'
+    },
+    {
+        title:'Nine Dead',
+        url:'https://www.youtube.com/watch?v=B0D9yqV3Aas'
+    },
+    {
+        title:'Survival of the Dead',
+        url:'https://www.youtube.com/watch?v=V_ELfwOkOps'
+    },
+    {
+        title:'Vanishing on 7th Street',
+        url:'https://www.youtube.com/watch?v=X-RA1U2rAw8'
+    },
+    {
+        title:'Captifs',
+        url:'https://www.youtube.com/watch?v=eWjK3q1Wxd4'
+    },
+    {
+        title:'Derrière les murs',
+        url:'https://www.youtube.com/watch?v=pwA9NuQn2Gg'
+    },
+    {
+        title:'The Exorcist',
+        url:'https://www.youtube.com/watch?v=VU939o_A1aI'
+    },
+    {
+        title:'The Hills Have Eyes 2',
+        url:'https://www.youtube.com/watch?v=ZCvzGoNpggY'
+    },
+    {
+        title:'Le Village des ombres',
+        url:'https://www.youtube.com/watch?v=kvd2VD12nbU'
+    },
+    {
+        title:'Territoires',
+        url:'https://www.youtube.com/watch?v=BAct3-JsAHY'
+    },
+    {
+        title:'Sept jours du Talion (les)',
+        url:'https://www.youtube.com/watch?v=mohfhoicHjI'
+    },
+    {
+        title:'Djinns',
+        url:'https://www.youtube.com/watch?v=vE9FKloroaE'
+    },
+    {
+        title:'Silent House (The)',
+        url:'https://www.youtube.com/watch?v=VD88EFBFuos'
+    },
+    {
+        title:'Night of the Demons',
+        url:'https://www.youtube.com/watch?v=MZy0gVlY304'
+    },
+    {
+        title:'Dream Home',
+        url:'https://www.youtube.com/watch?v=XFxIYqcmRxc'
+    },
+    {
+        title:'Two Eyes Staring',
+        url:'https://www.youtube.com/watch?v=kf_LJKCKf3E'
+    },
+    {
+        title:'Hatchet',
+        url:'https://www.youtube.com/watch?v=3C33S_cqiJ8'
+    },
+    {
+        title:'Hatchet 2',
+        url:'https://www.youtube.com/watch?v=Qmgd6_KzV_8'
+    },
+    {
+        title:'Propriété interdite',
+        url:'https://www.youtube.com/watch?v=W2yseHd0Oeg'
+    },
+    {
+        title:'Blood Island',
+        url:'https://www.youtube.com/watch?v=eBq0SLWNF-E'
+    },
+    {
+        title:'Necromentia',
+        url:'https://www.youtube.com/watch?v=ZbMOAXTv9D0'
+    },
+    {
+        title:'Pig',
+        url:'https://www.youtube.com/watch?v=1H9RjnXxUMA'
+    },
+    {
+        title:'Friit Vilt',
+        url:'https://www.youtube.com/watch?v=lBWRHD_i6nE'
+    },
+    {
+        title:'Friit Vilt 2',
+        url:'https://www.youtube.com/watch?v=PD2ykuNPJOQ'
+    },
+    {
+        title:'Friit Vilt 3',
+        url:'https://www.youtube.com/watch?v=z_ZwG1mKHRY'
+    },
+    {
+        title:'Invasion of the Not Quite Dead',
+        url:'https://www.youtube.com/watch?v=pQTgcAkvEzw'
+    },
+    {
+        title:'Web Cam',
+        url:'https://www.youtube.com/watch?v=U5iH86HrvuU'
+    },
+    {
+        title:'Cehennem',
+        url:'https://www.youtube.com/watch?v=A097nGgbPyw'
+    },
+    {
+        title:'Madness',
+        url:'https://www.youtube.com/watch?v=y4k8XWEpsNs'
+    },
+    {
+        title:'Shining',
+        url:'https://www.youtube.com/watch?v=S014oGZiSdI'
+    },
+    {
+        title:'Psychose',
+        url:'https://www.youtube.com/watch?v=Wz719b9QUqY'
+    },
+    {
+        title:'Dawn of the Dead',
+        url:'https://www.youtube.com/watch?v=Yd-z5wBeFTU'
+    },
+    {
+        title:'Night of the Living Dead',
+        url:'https://www.youtube.com/watch?v=ob8vZhSjES8'
+    },
+    {
+        title:'Carrie',
+        url:'https://www.youtube.com/watch?v=YuO26oJQLVs'
+    },
+    {
+        title:'Nosferatu',
+        url:'https://www.youtube.com/watch?v=ZxlJxDr26mM'
+    },
+    {
+        title:'The Birds',
+        url:'https://www.youtube.com/watch?v=0fJh2gIBOto'
+    },
+    {
+        title:'Martyrs',
+        url:'https://www.youtube.com/watch?v=5sXPBIA4zH4'
+    },
+    {
+        title:'Cannibal Holocaust',
+        url:'https://www.youtube.com/watch?v=HUDTPw-eSVs'
+    },
+    {
+        title:'The Blair Witch Project',
+        url:'https://www.youtube.com/watch?v=a_Hw4bAUj8A'
+    },
+    {
+        title:'Evil Dead',
+        url:'https://www.youtube.com/watch?v=NL6mioAlpJk'
+    },
+    {
+        title:'Poltergeist',
+        url:'https://www.youtube.com/watch?v=9eZgEKjYJqA'
+    },
+    {
+        title:'Eraserhead',
+        url:'https://www.youtube.com/watch?v=dU7OqGCIcak'
+    },
+    {
+        title:"Jacob's Ladder",
+        url:'https://www.youtube.com/watch?v=rJztRnDxdM8'
+    },
+    {
+        title:'28 Days Later...',
+        url:'https://www.youtube.com/watch?v=c7ynwAgQlDQ'
+    },
+    {
+        title:'28 Weeks Later...',
+        url:'https://www.youtube.com/watch?v=cbpjH4XCG3c'
+    },
+    {
+        title:'Insidious: Chapter 2',
+        url:'https://www.youtube.com/watch?v=fBbi4NeebAk'
+    },
+    
+]
+export default Trailer;
